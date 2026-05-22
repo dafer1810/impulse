3f6baf67bf4e305e65d229f6633bf59c7a89fa64
Mauricio Florez <florexmauricio15@gmail.com>
2026-05-22 16:34:02 -0500

    Se hace commit con la pantalla_de_carga.java and activity_pantalla_de_carga.xml modificados y en el archivo AndroidManifest.xml se habilito el activity_pantalla_inicio_de_sesion.xml

--- app/src/main/java/com/example/impulseapp/pantalla_de_carga.java
+++ app/src/main/java/com/example/impulseapp/pantalla_de_carga.java
@@ -1,12 +1,13 @@
 package com.example.impulseapp;
 
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class pantalla_de_carga extends AppCompatActivity {
 
@@ -15,10 +16,22 @@ public class pantalla_de_carga extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pantalla_de_carga);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+
+        // Animación de la barra: de 0 a 100 en 1000ms (1 segundo)
+        ObjectAnimator progressAnimator = ObjectAnimator.ofInt(progressBar, "progress", 0, 100);
+        progressAnimator.setDuration(1000);
+        progressAnimator.start();
+
+        // Handler para cambiar de pantalla después de 2 segundos
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(pantalla_de_carga.this, pantalla_inicio_de_sesion.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
